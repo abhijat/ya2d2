2240e5bb52f4f8283d7bf7656e977e9442dbb6d3
Abhijat Malviya <malviya.abhijat@gmail.com>
2018-12-01 21:43:05 +0530

    adds skeleton of push function

--- src/cmd/parser.rs
+++ src/cmd/parser.rs
@@ -1,14 +1,14 @@
+use self::ParseResponse::*;
+
 pub enum ParseResponse {
     ChangePrompt(String),
     DisplayString(String),
 }
 
 pub fn process_command(command: String) -> ParseResponse {
-    use self::ParseResponse::*;
-
     match command.trim_right().as_ref() {
         "ls" => DisplayString("heres your list".to_string()),
-        "push" => DisplayString("push away friend".to_string()),
+        s if s.starts_with("push") => parse_push_command(s),
         "pop" => DisplayString("pop away friendo".to_string()),
         s if s.starts_with("change-prompt") => parse_change_prompt_command(s),
         _ => DisplayString("unknown command, my good pal".to_string()),
@@ -16,8 +16,6 @@ pub fn process_command(command: String) -> ParseResponse {
 }
 
 fn parse_change_prompt_command(command: &str) -> ParseResponse {
-    use self::ParseResponse::*;
-
     let new_prompt = command.split_whitespace()
         .skip(1)
         .nth(0);
@@ -27,6 +25,20 @@ fn parse_change_prompt_command(command: &str) -> ParseResponse {
             ChangePrompt(prompt_string)
         }
         None => DisplayString("usage: change-prompt <new-prompt>".to_string()),
+    }
+}
+
+fn parse_push_command(command: &str) -> ParseResponse {
+    let payload = command.split_whitespace()
+        .skip(1)
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    if payload.is_empty() {
+        DisplayString("usage: push <description of the task>".to_string())
+    } else {
+        DisplayString(format!("Let's add the task '{}'", payload))
     }
 }
 
